1f97bb4ed79cab2a5d069ed334ccce4eb1c43f6f
Amit Alon <amit@kaholo.io>
2021-04-04 17:17:46 +0300

    minor fix and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # kaholo-plugin-vstest
-VSTest plugin for Kaholo
+VSTest plugin for Kaholo, uses command line.
+* Please Notice this plugin works only on agents with Windows OS.
+
+## Settings 
+1. VSTest EXE Path (String) **Required** - The path to the exe file of VSTest on the agent.
+
+## Method: Run Test
+Run a test or multiple tests. You can see here the command [documentation](https://docs.microsoft.com/en-us/visualstudio/test/vstest-console-options?view=vs-2019).
+
+### Parameters
+1. Test Files (Text/Array) **Required** - The path(s) to the test files to run. Can be passed either as text, using new lines to seperate multiple values, or as an array from code.
+2. Settings (String) **Optional** - Path to settings file that includes any additional settings to run tests with. For more information, see [Configure unit tests using a .runsettings file](https://docs.microsoft.com/en-us/visualstudio/test/configure-unit-tests-by-using-a-dot-runsettings-file?view=vs-2019).
+3. Tests (Text/Array) **Optional** - When provided run only tests with the names provided. Can be passed either as text, using new lines to seperate multiple values, or as an array from code.
+4. Enable Code Coverage (Boolean) **Optional** - Enables data diagnostic adapter CodeCoverage in the test run. Default used is according to default settings in VSTest.
+5. Parallel (Boolean) **Optional** - Specifies that the tests be executed in parallel. Default is false.
+6. In Isolation (Boolean) **Optional** - Runs the tests in an isolated process. This isolation makes the vstest.console.exe process less likely to be stopped on an error in the tests, but tests might run slower. Default is false.
+7. CLI Args (String) **Optional** - Any other cli argumants or flags to include in the command.
--- src/app.js
+++ src/app.js
@@ -1,68 +1,26 @@
-const child_process = require("child_process")
-
-function runTest(action, settings) {
-    return new Promise((resolve, reject) => {
-        var vstestPath = `"${settings.exePath}"`;
-        var testFiles = _handleStringArray(action.params.testFiles);
-        var cliArgs = action.params.cliArgs;
-
-        if (!Array.isArray(testFiles))
-            return reject("Test Files must be either a string or an array");
+const { handleStringArray, exec } = require("./helpers");
 
 
-        testFiles = testFiles.map(testFile=>{
-            return `"${testFile}"`
-        })
+async function runTest(action, settings) {
+    let args = handleStringArray(action.params.testFiles).map(testFile => `"${testFile}"`);
 
-        var inIsolation = action.params.inIsolation;
-        var parallel = action.params.parallel;
-        var testSettings = action.params.settings;
-        var testsNames = _handleStringArray(action.params.tests);
-        var enableCodeCoverage = action.params.enableCodeCoverage;
-
-
-        var args = [
-            testFiles.join(' ')
-        ];
-
-        if (testSettings) args.push(`/Settings:${testSettings}`)
-        if (testsNames) args.push(`/Tests:${testsNames.join(',')}`)
-        if (enableCodeCoverage) args.push('/Enablecodecoverage')
-        if (inIsolation) args.push('/InIsolation')
-        if (parallel) args.push('/Parallel')
-        if (cliArgs) args.push(cliArgs);
-
-        var command = `${vstestPath} ${args.join(' ')}`
-
-        child_process.exec(command, (error, stdout, stderr) => {
-            if (error) {
-                return reject(`exec error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-            }
-            return resolve(stdout);
-        });
-
-
-    });
-}
-
-/**
- * @param {*} value 
- * @returns {string[]}
- */
-function _handleStringArray(value) {
-    if (typeof value == 'string') {
-        return [value]
+    if (!args || !settings.exePath){ // args is actually test files
+        throw "One of the required parameters were not provided";
     }
-    return value;
-}
 
-function isObject(value) {
-    return !!(value && typeof value === 'object' && value.constructor === Object);
-}
+    const testSettings = (action.params.settings || "").trim();
+    const testsNames = handleStringArray(action.params.tests);
 
+    if (testSettings) args.push(`/Settings:${testSettings}`)
+    if (testsNames) args.push(`/Tests:${testsNames.join(',')}`)
+    if (action.params.enableCodeCoverage) args.push('/Enablecodecoverage')
+    if (action.params.inIsolation) args.push('/InIsolation')
+    if (action.params.parallel) args.push('/Parallel')
+    if (action.params.cliArgs) args.push(action.params.cliArgs);
+    const command = `"${settings.exePath}" ${args.join(' ')}`
+
+    return exec(command);
+}
 
 module.exports = {
     runTest: runTest
